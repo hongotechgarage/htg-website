576c678c74e611565f57482120de7b10319010c3
charon616 <milkyway.k16@gmail.com>
2020-06-03 01:11:49 +0900

    大まかなtb/spレイアウト

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,23 @@
-// let menu: HTMLElement = document.getElementById('menu');
-// let menu_trigger: HTMLElement = document.getElementById('menu-trigger');
-// if (!menu) {
-//     throw new Error('menuがありません！');
-// } 
-// if (!menu_trigger) {
-//     throw new Error('menu_triggerがありません！');
-// }  
-// menu_trigger.addEventListener('click', () => {
-//     menu_trigger.classList.toggle('active');
-//     if (menu.style.display === "none") {
-//         menu.style.display = "block";
-//     } else {
-//         menu.style.display = "none";
-//     }
-// }, false);
-// Barba.Pjax.start();
 "use strict";
+
+var menu = document.getElementById('menu');
+var menu_trigger = document.getElementById('menu-trigger');
+
+if (!menu) {
+  throw new Error('menuがありません！');
+}
+
+if (!menu_trigger) {
+  throw new Error('menu_triggerがありません！');
+}
+
+menu_trigger.addEventListener('click', function () {
+  menu_trigger.classList.toggle('active');
+
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}, false); // Barba.Pjax.start();
 //# sourceMappingURL=index.js.map
--- src/_ts/index.ts
+++ src/_ts/index.ts
@@ -1,18 +1,18 @@
-// let menu: HTMLElement = document.getElementById('menu');
-// let menu_trigger: HTMLElement = document.getElementById('menu-trigger');
-// if (!menu) {
-//     throw new Error('menuがありません！');
-// } 
-// if (!menu_trigger) {
-//     throw new Error('menu_triggerがありません！');
-// }  
-// menu_trigger.addEventListener('click', () => {
-//     menu_trigger.classList.toggle('active');
-//     if (menu.style.display === "none") {
-//         menu.style.display = "block";
-//     } else {
-//         menu.style.display = "none";
-//     }
-// }, false);
+let menu: HTMLElement = document.getElementById('menu');
+let menu_trigger: HTMLElement = document.getElementById('menu-trigger');
+if (!menu) {
+    throw new Error('menuがありません！');
+} 
+if (!menu_trigger) {
+    throw new Error('menu_triggerがありません！');
+}  
+menu_trigger.addEventListener('click', () => {
+    menu_trigger.classList.toggle('active');
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}, false);
 
 // Barba.Pjax.start();
