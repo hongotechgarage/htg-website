cdef36c6f46d0949b98a6b0282b5657d8d02e66b
charon616 <milkyway.k16@gmail.com>
2020-06-03 02:37:26 +0900

    topへ戻るボタン，ハンバーガーメニューの実装

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,56 @@
 "use strict";
 
 var menu = document.getElementById('menu');
-var menu_trigger = document.getElementById('menu-trigger');
+var menuTrigger = document.getElementById('menu-trigger'); // if (!menu) {
+//     throw new Error('menuがありません！');
+// } 
+// if (!menu_trigger) {
+//     throw new Error('menu_triggerがありません！');
+// }  
+// menu_trigger.addEventListener('click', () => {
+//     menu_trigger.classList.toggle('active');
+//     if (menu.style.display === "none") {
+//         menu.style.display = "block";
+//     } else {
+//         menu.style.display = "none";
+//     }
+// }, false);
 
-if (!menu) {
-  throw new Error('menuがありません！');
-}
+function toggleNav() {
+  menuTrigger.addEventListener('click', function () {
+    menuTrigger.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
 
-if (!menu_trigger) {
-  throw new Error('menu_triggerがありません！');
-}
-
-menu_trigger.addEventListener('click', function () {
-  menu_trigger.classList.toggle('active');
-
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
+  if (menuTrigger.classList.contains('active')) {
+    menuTrigger.classList.remove('active');
+    menu.classList.remove('active');
   }
-}, false); // Barba.Pjax.start();
+}
+
+toggleNav();
+var topButton = document.getElementById('top-button');
+
+function getScrolled() {
+  return window.pageYOffset !== undefined ? window.pageYOffset : document.documentElement.scrollTop;
+}
+
+function scrollToTop() {
+  var scrolled = getScrolled();
+  window.scrollTo(0, Math.floor(scrolled / 2));
+
+  if (scrolled > 0) {
+    window.setTimeout(scrollToTop, 80);
+  }
+}
+
+;
+
+window.onscroll = function () {
+  getScrolled() > 500 ? topButton.classList.add('fade-in') : topButton.classList.remove('fade-in');
+};
+
+topButton.onclick = function () {
+  scrollToTop();
+};
 //# sourceMappingURL=index.js.map
--- src/_ts/index.ts
+++ src/_ts/index.ts
@@ -1,18 +1,51 @@
 let menu: HTMLElement = document.getElementById('menu');
-let menu_trigger: HTMLElement = document.getElementById('menu-trigger');
-if (!menu) {
-    throw new Error('menuがありません！');
-} 
-if (!menu_trigger) {
-    throw new Error('menu_triggerがありません！');
-}  
-menu_trigger.addEventListener('click', () => {
-    menu_trigger.classList.toggle('active');
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}, false);
+let menuTrigger: HTMLElement = document.getElementById('menu-trigger');
+// if (!menu) {
+//     throw new Error('menuがありません！');
+// } 
+// if (!menu_trigger) {
+//     throw new Error('menu_triggerがありません！');
+// }  
+// menu_trigger.addEventListener('click', () => {
+//     menu_trigger.classList.toggle('active');
+//     if (menu.style.display === "none") {
+//         menu.style.display = "block";
+//     } else {
+//         menu.style.display = "none";
+//     }
+// }, false);
 
-// Barba.Pjax.start();
+function toggleNav() {
+    menuTrigger.addEventListener('click', function () {
+        menuTrigger.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+    if (menuTrigger.classList.contains('active')) {
+        menuTrigger.classList.remove('active');
+        menu.classList.remove('active');
+    }
+}
+toggleNav();
+
+
+let topButton: HTMLElement = document.getElementById('top-button');
+
+function getScrolled() {
+    return (window.pageYOffset !== undefined) ? window.pageYOffset : document.documentElement.scrollTop;
+}
+
+function scrollToTop() {
+    const scrolled = getScrolled();
+    window.scrollTo(0, Math.floor(scrolled / 2));
+    if (scrolled > 0) {
+        window.setTimeout(scrollToTop, 80);
+    }
+};
+
+window.onscroll = function () {
+    (getScrolled() > 500) ? topButton.classList.add('fade-in') : topButton.classList.remove('fade-in');
+};
+
+topButton.onclick = function () {
+    scrollToTop();
+};
